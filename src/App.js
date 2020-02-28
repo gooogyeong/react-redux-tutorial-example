8440a4f6ab0cb1e717a322aa11e134195796043f
Minkyung Lee <gooogyeong@gmail.com>
2020-02-28 13:14:51 +0900

    no division complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, {Component} from 'react';
-import './App.css';
-import AddNumberRoot from "./components/AddNumberRoot";
-import DisplayNumberRoot from "./components/DisplayNumberRoot";
+import React, { Component } from "react";
+import "./App.css";
+// import AddNumberRoot from "./components/AddNumberRoot";
+// import DisplayNumberRoot from "./components/DisplayNumberRoot";
+import Addnumber from "./containers/AddNumber.jsx";
+import DisplayNumber from "./containers/DisplayNumber.jsx";
 
 class App extends Component {
-  state = {number:0}
-  render(){
+  state = { number: 0 };
+  render() {
     return (
       <div className="App">
         <h1>Root</h1>
-        <AddNumberRoot></AddNumberRoot>
-        <DisplayNumberRoot></DisplayNumberRoot>
+        <Addnumber />
+        <DisplayNumber />
+        {/* <AddNumberRoot></AddNumberRoot>
+        <DisplayNumberRoot></DisplayNumberRoot> */}
       </div>
-    );  
+    );
   }
 }
 
--- src/containers/AddNumber.jsx
+++ src/containers/AddNumber.jsx
@@ -1,21 +1,52 @@
-import AddNumber from "../components/AddNumber";
-import {connect} from 'react-redux';
-function mapDispatchToProps(dispatch){
-    return {
-        onClick:function(size){
-            dispatch({type:'INCREMENT', size:size});
-        }
-    }
-}
-export default connect(null, mapDispatchToProps)(AddNumber);
-/*
+// import AddNumber from "../components/AddNumber";
+// import {connect} from 'react-redux';
+// function mapDispatchToProps(dispatch){
+//     return {
+//         onClick:function(size){
+//             dispatch({type:'INCREMENT', size:size});
+//         }
+//     }
+// }
+// export default connect(null, mapDispatchToProps)(AddNumber);
+
+// import React, { Component } from "react";
+
+// export default class AddNumber extends Component {
+//   state = {size:1}
+//   render() {
+//     return (
+//       <AddNumber
+//         onClick={function(size) {
+//           ;
+//         }.bind(this)}
+//       ></AddNumber>
+//     );
+//   }
+// }
+
 import React, { Component } from "react";
-import store from '../store';
-export default class extends Component{
-    render(){
-        return <AddNumber onClick={function(size){
-            store.dispatch({type:'INCREMENT', size:size});
-        }.bind(this)}></AddNumber>
-    }
+import store from "../store";
+export default class AddNumber extends Component {
+  state = { size: 1 };
+  render() {
+    return (
+      <div>
+        <h1>Add Number</h1>
+        <input
+          type="button"
+          value="+"
+          onClick={function() {
+            store.dispatch({ type: "INCREMENT", size: this.state.size });
+          }.bind(this)}
+        ></input>
+        <input
+          type="text"
+          value={this.state.size}
+          onChange={function(e) {
+            this.setState({ size: Number(e.target.value) });
+          }.bind(this)}
+        ></input>
+      </div>
+    );
+  }
 }
-*/
